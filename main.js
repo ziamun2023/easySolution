f6b44b17ac839fa46a5111e0affcfd3e03c5a5da
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-08-18 15:50:41 +0600

    this is second section and navlink added

--- main.js
+++ main.js
@@ -8,11 +8,27 @@ $(document).ready(function() {
 
         $("html, body").animate({ scrollTop: targetPosition }, 1500); // Smooth scroll to the target
     });
+    $(window).scroll(function() {
+        var currentPosition = $(window).scrollTop(); // Get the current scroll position
+
+        $(".navbar a").each(function() {
+            var targetSection = $(this).attr("href"); // Get the target section ID
+            var targetPosition = $(targetSection).offset().top;
+            var sectionHeight = $(targetSection).outerHeight(); // Get the height of the section
+
+            // Check if the current scroll position is within the bounds of the section
+            if (currentPosition >= targetPosition && currentPosition < targetPosition + sectionHeight) {
+                $(".navbar a").removeClass("active"); // Remove 'active' class from all navbar links
+                $(this).addClass("active"); // Add 'active' class to the current navbar link
+            }
+        });
+    });
+ 
 
     $('.downArrow').click(function(){
        var targetsection= $(this).data("target")
        console.log(targetsection)
        var targetposition=$(targetsection).offset().top
-       $('html,body').animate({scrollTop :targetposition},800)
+       $('html,body').animate({scrollTop :targetposition},1200)
     })
 });
